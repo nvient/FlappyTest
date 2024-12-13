81c908d9b50231b958079018324b87e514484889
nvient <126623132+nvient@users.noreply.github.com>
2024-12-13 14:27:52 -0500

    Update flappyphish.js

--- flappyphish.js
+++ flappyphish.js
@@ -56,7 +56,7 @@ function resizeCanvas() {
   scaleX = canvas.width / referenceWidth;
   scaleY = canvas.height / referenceHeight;
 
-  fishWidth = canvas.width / 8;
+  fishWidth = canvas.width / 12;
   fishHeight = fishWidth * (fish.height / fish.width);
   fishX = canvas.width / 4;
   fishY = canvas.height / 2;
