7970728575e753d0d486d3689df756a7ab6e0f8e
nvient <126623132+nvient@users.noreply.github.com>
2024-12-18 10:07:22 -0500

    Update flappyphish.js

--- flappyphish.js
+++ flappyphish.js
@@ -5,7 +5,7 @@ const overlay = document.getElementById("overlay");
 
 console.log("Start button:", document.getElementById("startButton"));
   
-let gravity = 0.1;
+let gravity = 0.2;
 let jumpHeight = 25;
 let gameRunning = false; // Initialize game as not running
 let gameOver = false; 
